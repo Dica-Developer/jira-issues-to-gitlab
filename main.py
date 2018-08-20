798b15ce9ebdeb774e37de6a2d8b02b68733e6f0
Ülgen Sarıkavak <ulgensrkvk@gmail.com>
2018-08-20 17:52:12 +0300

    Refactor

--- main.py
+++ main.py
@@ -1,63 +1,104 @@
-import requests
-from requests.auth import HTTPBasicAuth
-import re
-from StringIO import StringIO
+import configparser
+import sys
+from io import StringIO
 
-JIRA_URL = 'https://your-jira-url.tld/'
-JIRA_ACCOUNT = ('jira-username', 'jira-password')
-# the JIRA project ID (short)
-JIRA_PROJECT = 'PRO'
-GITLAB_URL = 'http://your-gitlab-url.tld/'
-# this token will be used whenever the API is invoked and
-# the script will be unable to match the jira's author of the comment / attachment / issue
-# this identity will be used instead.
-GITLAB_TOKEN = 'get-this-token-from-your-profile'
-# the project in gitlab that you are importing issues to.
-GITLAB_PROJECT = 'namespaced/project/name'
-# the numeric project ID. If you don't know it, the script will search for it
-# based on the project name.
-GITLAB_PROJECT_ID = None
-# set this to false if JIRA / Gitlab is using self-signed certificate.
-VERIFY_SSL_CERTIFICATE = True
+import requests
+from urllib.parse import quote
+from requests.auth import HTTPBasicAuth
+
+
+config = configparser.ConfigParser(allow_no_value=True)
+config.read("conf.ini")
+
+JIRA_URL = config['jira']['url']
+JIRA_USERNAME = config['jira']['username']
+JIRA_PASSWORD = config['jira']['password']
+JIRA_PROJECT = config['jira']['project']
+
+GITLAB_URL = config['gitlab']['url']
+GITLAB_TOKEN = config['gitlab']['token']
+GITLAB_PROJECT = config['gitlab']['project']
+GITLAB_PROJECT_ID = config['gitlab']['id']
 
 # IMPORTANT !!!
 # make sure that user (in gitlab) has access to the project you are trying to
 # import into. Otherwise the API request will fail.
 
-# jira user name as key, gitlab as value
 # if you want dates and times to be correct, make sure every user is (temporarily) admin
+
+# jira user name as key, gitlab as value
 GITLAB_USER_NAMES = {
-    'jira': 'gitlab',
+    'ulgensrkvk': 'ulgens',
 }
 
-jira_issues = requests.get(
+# Test Jira connection
+print("Testing Jira connection...")
+try:
+    jira_connection = requests.get(
+        JIRA_URL,
+        auth=HTTPBasicAuth(JIRA_USERNAME, JIRA_PASSWORD),
+        headers={'Content-Type': 'application/json'}
+    )
+except ConnectionError:
+    raise ConnectionError(f"Couldn't connect to {JIRA_URL}")
+
+if jira_connection.status_code != 200:
+    print("Jira connection cannot be established!")
+    print(f"{jira_connection.status_code}: {jira_connection.reason}")
+    sys.exit(1)
+else:
+    print("Jira connection was successful!")
+
+print("Fetching Jira issues...")
+jira_issues_request = requests.get(
     JIRA_URL + 'rest/api/2/search?jql=project=%s+&maxResults=10000' % JIRA_PROJECT,
-    auth=HTTPBasicAuth(*JIRA_ACCOUNT),
-    verify=VERIFY_SSL_CERTIFICATE,
+    auth=HTTPBasicAuth(JIRA_USERNAME, JIRA_PASSWORD),
     headers={'Content-Type': 'application/json'}
 )
 
+if jira_issues_request.status_code != 200:
+    for message in jira_issues_request.json()["errorMessages"]:
+        print(message)
+    sys.exit(1)
+else:
+    jira_issues = jira_issues_request.json()["issues"]
+    print(f"Found {len(jira_issues)} in project {JIRA_PROJECT}")
+
+
+# Test Gitlab connection
+gitlab_connection = requests.get(
+    GITLAB_URL + 'api/v4/projects',
+    headers={'PRIVATE-TOKEN': GITLAB_TOKEN}
+)
+
+if gitlab_connection.status_code != 200:
+    print("Gitlab connection cannot be established!")
+    print(f"{gitlab_connection.status_code}: {gitlab_connection.reason}")
+    sys.exit(1)
+
+# Find out the ID of the project
 if not GITLAB_PROJECT_ID:
-    # find out the ID of the project.
-    for project in requests.get(
-            GITLAB_URL + 'api/v3/projects',
-            headers={'PRIVATE-TOKEN': GITLAB_TOKEN},
-    ).json():
-        if project['path_with_namespace'] == GITLAB_PROJECT:
-            GITLAB_PROJECT_ID = project['id']
-            break
+    gitlab_project_connection = requests.get(
+        GITLAB_URL + f'api/v4/projects/{quote(GITLAB_PROJECT, safe="")}',
+        headers={'PRIVATE-TOKEN': GITLAB_TOKEN}
+    )
 
-if not GITLAB_PROJECT_ID:
-    raise Exception("Unable to find %s in gitlab!" % GITLAB_PROJECT)
+    if gitlab_project_connection.status_code != 200:
+        print(f"{gitlab_project_connection.status_code}: {gitlab_project_connection.reason}")
+        sys.exit(1)
+    else:
+        gitlab_project_id = gitlab_project_connection.json()["id"]
+        gitlab_project_name = gitlab_project_connection.json()["path_with_namespace"]
+        print(f"Project found: {gitlab_project_name} {gitlab_project_id }")
 
-for issue in jira_issues.json()['issues']:
 
+for issue in jira_issues:
     reporter = issue['fields']['reporter']['name']
+    print(reporter)
 
     gl_issue = requests.post(
-        GITLAB_URL + 'api/v3/projects/%s/issues' % GITLAB_PROJECT_ID,
-        headers={'PRIVATE-TOKEN': GITLAB_TOKEN, 'SUDO': GITLAB_USER_NAMES.get(reporter, reporter)},
-        verify=VERIFY_SSL_CERTIFICATE,
+        GITLAB_URL + f'api/v4/projects/{quote(GITLAB_PROJECT, safe="")}/issues',
+        headers={'PRIVATE-TOKEN': GITLAB_TOKEN},
         data={
             'title': issue['fields']['summary'],
             'description': issue['fields']['description'],
@@ -68,8 +109,7 @@ for issue in jira_issues.json()['issues']:
     # get comments and attachments
     issue_info = requests.get(
         JIRA_URL + 'rest/api/2/issue/%s/?fields=attachment,comment' % issue['id'],
-        auth=HTTPBasicAuth(*JIRA_ACCOUNT),
-        verify=VERIFY_SSL_CERTIFICATE,
+        auth=HTTPBasicAuth(JIRA_USERNAME, JIRA_PASSWORD),
         headers={'Content-Type': 'application/json'}
     ).json()
 
@@ -78,8 +118,7 @@ for issue in jira_issues.json()['issues']:
 
         note_add = requests.post(
             GITLAB_URL + 'api/v3/projects/%s/issues/%s/notes' % (GITLAB_PROJECT_ID, gl_issue),
-            headers={'PRIVATE-TOKEN': GITLAB_TOKEN, 'SUDO': GITLAB_USER_NAMES.get(author, author)},
-            verify=VERIFY_SSL_CERTIFICATE,
+            headers={'PRIVATE-TOKEN': GITLAB_TOKEN},
             data={
                 'body': comment['body'],
                 'created_at': comment['created']
@@ -92,22 +131,20 @@ for issue in jira_issues.json()['issues']:
 
             _file = requests.get(
                 attachment['content'],
-                auth=HTTPBasicAuth(*JIRA_ACCOUNT),
-                verify=VERIFY_SSL_CERTIFICATE,
+                auth=HTTPBasicAuth(JIRA_USERNAME, JIRA_PASSWORD),
             )
 
             _content = StringIO(_file.content)
 
             file_info = requests.post(
                 GITLAB_URL + 'api/v3/projects/%s/uploads' % GITLAB_PROJECT_ID,
-                headers={'PRIVATE-TOKEN': GITLAB_TOKEN, 'SUDO': GITLAB_USER_NAMES.get(author, author)},
+                headers={'PRIVATE-TOKEN': GITLAB_TOKEN},
                 files={
                     'file': (
                         attachment['filename'],
                         _content
                     )
                 },
-                verify=VERIFY_SSL_CERTIFICATE
             )
 
             del _content
@@ -116,14 +153,13 @@ for issue in jira_issues.json()['issues']:
             # attachment
             requests.post(
                 GITLAB_URL + 'api/v3/projects/%s/issues/%s/notes' % (GITLAB_PROJECT_ID, gl_issue),
-                headers={'PRIVATE-TOKEN': GITLAB_TOKEN, 'SUDO': GITLAB_USER_NAMES.get(author, author)},
-                verify=VERIFY_SSL_CERTIFICATE,
+                headers={'PRIVATE-TOKEN': GITLAB_TOKEN},
                 data={
                     'body': file_info.json()['markdown'],
                     'created_at': attachment['created']
                 }
             )
 
-    print "created issue #%s" % gl_issue
+    print("created issue #%s" % gl_issue)
 
-print "imported %s issues from project %s" % (len(jira_issues.json()['issues']), JIRA_PROJECT)
+print("imported %s issues from project %s" % (len(jira_issues), JIRA_PROJECT))
